903357dda87b291a9b55527a706b11840fdd96bb
CharlesGarrocho <charles.garrocho@gmail.com>
2013-04-15 10:37:20 -0300

    adicionado: arquivo padrão do módulo do software.

---
+++ source/githubnotifi/__init__.py
@@ -0,0 +1,16 @@
+# coding: utf-8
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2012-2013 Python Software Open Source
+
+"""
+Este é o pacote do projeto GitHubNotifi.
+"""
+
+__version__ = '0.1'
+__author__  = 'Charles Tim Batista Garrocho'
+
+__all__ = [
+    'gui',
+    'settings',
+]
