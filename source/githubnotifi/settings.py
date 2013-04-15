c4e2571e9c467d70ade0b2dcfcaf523e5ac7249a
CharlesGarrocho <charles.garrocho@gmail.com>
2013-04-15 10:36:45 -0300

    adicionado: arquivo de configurações do software.

---
+++ source/githubnotifi/settings.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+# @author: Charles Tim Batista Garrocho
+# @contact: charles.garrocho@gmail.com
+# @copyright: (C) 2012-2013 Python Software Open Source
+
+"""
+Modulo responsavel pelas configuracoes globais do software.
+"""
